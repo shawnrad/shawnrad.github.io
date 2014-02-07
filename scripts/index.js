c2114cc1f61cb25890994a16e7b191437dc6bc94
shawnrad <shawnrad@alum.mit.edu>
2014-02-07 14:39:29 -0800

    Zoom CSS

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,48 @@ $(document).ready(function(){
 });
 
 /*
-	Increase/Decrease SVG text by +/- scale
+	Increase/Decrease by CSS
 */
-function SVG_zoom(scale){
-	fontsize = parseInt($("#svg_text").attr("font-size"));
-	$("#svg_text").attr("font-size", Math.max(1,fontsize+scale));
+function CSS_zoom(selector, scale) {
+	var MIN_SCALE = 0.2;
+	
+	try {
+		matrix = $.map($(selector).css("transform").split("(")[1].split(")")[0].split(","), function(el,index){return parseFloat(el);});
+		$(selector).css("transform", "scale("+Math.max(MIN_SCALE,matrix[0] + scale)+","+Math.max(MIN_SCALE,matrix[3] + scale)+")");
+	}
+	catch(err){/*CSS3 not supported*/}
+  
+	try {
+		matrix = $.map($(selector).css("-ms-transform").split("(")[1].split(")")[0].split(","), function(el,index){return parseFloat(el);});
+		$(selector).css("-ms-transform", "scale("+Math.max(MIN_SCALE,matrix[0] + scale)+","+Math.max(MIN_SCALE,matrix[3] + scale)+")");
+	}
+	catch(err){/* not IE9 */}
+	
+	try {
+		matrix = $.map($(selector).css("-webkit-transform").split("(")[1].split(")")[0].split(","), function(el,index){return parseFloat(el);});
+		$(selector).css("-webkit-transform", "scale("+Math.max(MIN_SCALE,matrix[0] + scale)+","+Math.max(MIN_SCALE,matrix[3] + scale)+")");
+	}
+	catch(err){/* Safari and Chrome */}
 }
+
+
+/*
+	Map keypresses to actions
+*/
+function map_controls(){    
+    $(document).keypress(function (event) {
+        switch (event.charCode) {
+            case (50): // 2
+                CSS_zoom("div",-.2);
+                break;
+            case (56): // 8
+                CSS_zoom("div",.2);
+                break;
+        }
+    });
+};
+
+
 
 /*
 	Set background as input from camera.
@@ -24,22 +60,6 @@ function background_video(){
 	  //DO NOTHING
 	}
 }
-
-/*
-	Map keypresses to actions
-*/
-function map_controls(){    
-    $(document).keypress(function (event) {
-        switch (event.charCode) {
-            case (50): // 2
-                SVG_zoom(-5);
-                break;
-            case (56): // 8
-                SVG_zoom(5)
-                break;
-        }
-    });
-};
 
 /**
 * UTILITIES
