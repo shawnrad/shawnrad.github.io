84599091a70f545490d2f59a831ca7a4a3639f3b
shawnrad <shawnrad@alum.mit.edu>
2014-02-22 22:32:25 -0800

    HTML5 Orientation Demo
    See
    http://www.html5rocks.com/en/tutorials/device/orientation/deviceorientationsample.html

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,62 @@
 	On Ready
 */
 $(document).ready(function(){
+	initialization();
 	background_video();
 	map_controls();
 });
+
+var count = 0;
+function initialization(){
+	if (!window.DeviceOrientationEvent) {
+		$("#infobar").text("DeviceOrientation is not supported").css("background","#FF6666");
+		console.warn("DeviceOrientation is not supported");
+	}
+	else{ 
+		init()
+	}
+
+}
+
+function init() {
+  if (window.DeviceOrientationEvent) {
+	document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+	// Listen for the deviceorientation event and handle the raw data
+	window.addEventListener('deviceorientation', function(eventData) {
+	  // gamma is the left-to-right tilt in degrees, where right is positive
+	  var tiltLR = eventData.gamma;
+	  
+	  // beta is the front-to-back tilt in degrees, where front is positive
+	  var tiltFB = eventData.beta;
+	  
+	  // alpha is the compass direction the device is facing in degrees
+	  var dir = eventData.alpha
+	  
+	  // call our orientation event handler
+	  deviceOrientationHandler(tiltLR, tiltFB, dir);
+	  }, false);
+  } else {
+	document.getElementById("doEvent").innerHTML = "Not supported on your device or browser.  Sorry."
+  }
+}
+
+function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  document.getElementById("doDirection").innerHTML = Math.round(dir);
+  
+  // Apply the transform to the image
+  var logo = document.getElementById("imgLogo");
+  logo.style.webkitTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+  logo.style.MozTransform = "rotate("+ tiltLR +"deg)";
+  logo.style.transform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+}
+
+
+// Some other fun rotations to try...
+//var rotation = "rotate3d(0,1,0, "+ (tiltLR*-1)+"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+//var rotation = "rotate("+ tiltLR +"deg) rotate3d(0,1,0, "+ (tiltLR*-1)+"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+
 
 /*
 	Increase/Decrease by CSS
@@ -49,15 +102,15 @@ function map_controls(){
 };
 
 
-
 /*
 	Set background as input from camera.
 */
 function background_video(){
 	if (hasGetUserMedia()) {
-	  
+		//DO SOMETHING WITH GETUSERMEDIA
 	} else {
-	  //DO NOTHING
+		$("#infobar").text("getUserMedia not supported.").css("background","#FF6666");
+		console.warn("getUserMedia is not supported");
 	}
 }
 
